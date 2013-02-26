20fe9038d146103ad7ceafc90814ab9237aa0974
Anthony Roberts <nns.aroberts@gmail.com>
2013-02-26 16:48:03 +0000

    django registration

--- apps/polls/models.py
+++ apps/polls/models.py
@@ -7,36 +7,6 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 
 
-
-class Editor(User):
-    """ Editor User Profile """
-    # user        = models.OneToOneField(User, related_name='editor', unique=True)
-    can_publish = models.BooleanField(default=True)
-    bio         = models.CharField(max_length=2000)
-    # notes       = models.ForeignKey(Notes)
-
-class Reporter(User):
-    """ Reporter User Profile """
-    # user        = models.OneToOneField(User, related_name='reporter', unique=True)
-    bio         = models.CharField(max_length=2000)
-    byline      = models.CharField(max_length=75)
-    # notes       = models.ForeignKey(Notes)
-
-
-class Client(User):
-    """ Client User Profile """
-    # user        = models.OneToOneField(User, related_name='client')
-    address     = models.CharField(max_length = 100)
-    city        = models.CharField(max_length=100)
-    state       = USStateField()
-    zipcode     = models.CharField(max_length=5)
-    pub_area    = models.CharField(max_length=100)
-    phone       = models.CharField(max_length=10)
-    twitter     = models.CharField(max_length=150)
-    facebook    = models.CharField(max_length=150)
-    website     = models.URLField(max_length=200)
-    # notes       = models.ForeignKey(Notes)
-
 class Poll(models.Model):
     question = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
@@ -54,10 +24,63 @@ class Choice(models.Model):
     votes = models.IntegerField()
     def __unicode__(self):
         return self.choice
-"""
-def create_editor_profile(sender, instance, created, **kwargs):
-    if created:
-        Editor.objects.create(user=instance)
 
-post_save.connect(create_editor_profile, sender=User)
-"""
+    
+USER_TYPES = (
+    ('Editor', 'Editor'),
+    ('Reporter', 'Reporter'),
+    ('Client', 'Client'),
+)
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, unique=True)
+    about = models.TextField(blank=True)
+    user_type = models.CharField(max_length=10, choices=USER_TYPES, default='Client')
+
+    def save(self, *args, **kwargs):
+        try:
+           existing = Profile.objects.get(user=self.user)
+           self.id = existing.id  #force update instead of insert 
+        except Profile.DoesNotExist:
+           print "Profile not created yet"
+        models.Model.save(self, *args, **kwargs) 
+
+def create_user(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+post_save.connect(create_user, sender=Profile)
+
+class Editor(User):
+    """ Editor User Profile """
+    # user        = models.OneToOneField(User, related_name='editor', unique=True)
+    can_publish = models.BooleanField(default=True)
+    bio         = models.CharField(max_length=2000)
+    # notes       = models.ForeignKey(Notes)
+    def __unicode__(self):
+        return self.last_name
+
+class Reporter(User):
+    """ Reporter User Profile """
+    # user        = models.OneToOneField(User, related_name='reporter', unique=True)
+    bio         = models.CharField(max_length=2000)
+    byline      = models.CharField(max_length=75)
+    # notes       = models.ForeignKey(Notes)
+    def __unicode__(self):
+        return self.last_name
+
+class Client(User):
+    """ Client User Profile """
+    # user        = models.OneToOneField(User, related_name='client')
+    address     = models.CharField(max_length = 100)
+    city        = models.CharField(max_length=100)
+    state       = USStateField()
+    zipcode     = models.CharField(max_length=5)
+    pub_area    = models.CharField(max_length=100)
+    phone       = models.CharField(max_length=10)
+    twitter     = models.CharField(max_length=150)
+    facebook    = models.CharField(max_length=150)
+    website     = models.URLField(max_length=200)
+    # notes       = models.ForeignKey(Notes)
+    def __unicode__(self):
+        return self.last_name
--- apps/urls.py
+++ apps/urls.py
@@ -2,13 +2,12 @@ from django.conf.urls.defaults import patterns, include, url
 from django.views.generic.simple import redirect_to, direct_to_template
 from django.conf import settings
 from django.contrib import admin
-
-
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', direct_to_template, { "template" : "welcome.html"}),
     url(r'^examples/', include('apps.examples.urls')),
+    url(r'^accounts/', include('registration.backends.default.urls')),
     url(r'^polls/', include('apps.polls.urls')),
     # url(r'^nns/', include('apps.nns.urls')),
     url(r'^admin/', include(admin.site.urls)),                   
