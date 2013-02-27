00d208ed4142e1370e8fe070e7f77fefff7ca21d
Anthony Roberts <nns.aroberts@gmail.com>
2013-02-27 19:44:29 +0000

    add wiki app

--- apps/urls.py
+++ apps/urls.py
@@ -6,11 +6,14 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', direct_to_template, { "template" : "welcome.html"}),
-    url(r'^examples/', include('apps.examples.urls')),
     url(r'^accounts/', include('registration.backends.default.urls')),
+    url(r'^admin/', include(admin.site.urls)), 
+    url(r'^examples/', include('apps.examples.urls')),
     url(r'^polls/', include('apps.polls.urls')),
+    url(r'^wiki/', include('apps.wiki.urls'))
     # url(r'^nns/', include('apps.nns.urls')),
-    url(r'^admin/', include(admin.site.urls)),                   
+                       
+                      
 )
 
 if getattr(settings,"DEBUG"):
