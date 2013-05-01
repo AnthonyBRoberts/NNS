0dd59cd9698b8fe832af1797b6201b79c631ae6d
Anthony Roberts <nns.aroberts@gmail.com>
2013-05-01 16:39:06 +0000

    send email task fix, hirefire Heroku addon install

--- apps/story/tasks.py
+++ apps/story/tasks.py
@@ -5,6 +5,10 @@ from apps.profiles.models import Client
 
 @task(name='send-email')
 def send_published_article(sender, subject, body):
+    """
+    Task for emailing published articles.
+    Runs when an article is saved and is_published==True
+    """
     for client in Client.objects.all():
         recipient = client.email
         send_mail(subject, body, sender, [recipient], fail_silently=False)
