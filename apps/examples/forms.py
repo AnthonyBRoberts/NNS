85c0176a972f1aac2b3d9607d0cbf3f2555e052a
Anthony Roberts <nns.aroberts@gmail.com>
2013-05-09 15:14:14 +0000

    cleanup unused app dirs

--- apps/examples/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-class EmailForm(forms.Form):
-    subject = forms.CharField(max_length=100)
-    message = forms.CharField(widget=forms.Textarea)
-    sender = forms.EmailField()
-    recipient = forms.EmailField()
--- apps/examples/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls.defaults import patterns, include, url
-from django.views.generic.simple import redirect_to, direct_to_template
-
-
-urlpatterns = patterns('',
-    url(r'^email/$', 'apps.examples.views.email', name='email_example'),
-)
