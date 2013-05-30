783066fbcb45d9089d1289fea3c1fd751213a4f2
Anthony Roberts <nns.aroberts@gmail.com>
2013-05-30 21:51:53 +0000

    TinyMCE in admin, profiles bug fix

--- apps/account/urls.py
+++ apps/account/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import *
 from account.models import *
 from account.forms import *
-from profiles import views
+from account import views
 
 
 urlpatterns = patterns('',
@@ -11,16 +11,19 @@ urlpatterns = patterns('',
     url('^client/create', 'profiles.views.create_profile',
         {
           'form_class': ClientForm,
+          'success_url': 'profiles_profile_detail',
         },
         name='create_client_profile'),
     url('^client/edit', 'profiles.views.edit_profile',
         {
             'form_class': ClientForm,
+            'success_url': '/profiles/client/edit/',
         },
         name='edit_client_profile'),
     url('^reporter/edit', 'profiles.views.edit_profile',
         {
             'form_class': ReporterForm,
+            'success_url': '/profiles/reporter/edit/',
         },
         name='edit_reporter_profile'),                 
     url(r'^create/$',
--- apps/story/admin.py
+++ apps/story/admin.py
@@ -1,5 +1,29 @@
 from django.contrib import admin
+from django.forms import *
+from django.db.models import *
 from models import Article, Edit
+from tinymce.widgets import TinyMCE
 
-admin.site.register(Article)
-admin.site.register(Edit)
+class EditInline(admin.TabularInline):
+    model = Edit
+
+class ArticleAdminForm(forms.ModelForm):
+    text = forms.CharField(widget=TinyMCE())
+    class Meta:
+        model = Article
+    
+class ArticleAdmin(admin.ModelAdmin):
+    form = ArticleAdminForm
+    fieldsets = [
+        (None, {'classes': ['edit'], 
+            'fields': (('title', 'slug'), 'author',
+                       ('is_published', 'publish_date'),
+                       'docfile', 'text'),
+            }),
+    ]
+    class Media:
+        js = ['/static/js/tinymce/tinymce.min.js', 'static/js/tinymce/textareas.js']
+    inlines = [EditInline]
+
+admin.site.register(Article, ArticleAdmin)
+
