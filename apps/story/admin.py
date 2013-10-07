db37fee097bbe74d3380d1bef492054556f72f6c
Anthony Roberts <nns.aroberts@gmail.com>
2013-10-07 20:59:09 +0000

    filter author list in story admin change form

--- apps/story/admin.py
+++ apps/story/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.db.models import Q
 from django.forms import *
 from django.forms import ModelForm
 from django.contrib.admin import ModelAdmin
 from suit_redactor.widgets import RedactorWidget
 from django.db.models import *
 from models import Article
+from apps.account.models import UserProfile
 from tinymce.widgets import TinyMCE
 from taggit.models import Tag, TaggedItem
 
@@ -12,12 +15,18 @@ class TagInline(admin.TabularInline):
     model = Tag
 
 class ArticleAdminForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(ArticleAdminForm, self).__init__(*args, **kwargs)
+        self.fields['author'].queryset = UserProfile.objects.filter(Q(user_type = 'Reporter') | Q(user_type = 'Editor'))
     class Meta:
         widgets = {
             'email_text': RedactorWidget(editor_options={'lang': 'en'}),
             'text': RedactorWidget(editor_options={'lang': 'en'}),
         }
         model = Article
+
+
+
     
 class ArticleAdmin(ModelAdmin):
     form = ArticleAdminForm
@@ -28,11 +37,11 @@ class ArticleAdmin(ModelAdmin):
     fieldsets = [
         (None, {'classes': ['edit'], 
             'fields': (('title', 'slug'), 'author',
-                       ('is_published', 'publish_date', 'send_now'),
+                       ('publish_date', 'is_published', 'send_now'),
                        'docfile', 'text', 'email_text'),
             }),
     ]
-    #inlines = (TagInline, )
+
 
 admin.site.register(Article, ArticleAdmin)
 
