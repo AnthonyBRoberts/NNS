2419d3e0a7e5f159cf7b846d78c29d44e9f69e60
Anthony Roberts <nns.aroberts@gmail.com>
2013-11-20 15:16:48 +0000

    revert send-email task wait to 1 second

--- apps/story/tasks.py
+++ apps/story/tasks.py
@@ -31,4 +31,4 @@ def send_published_article(bc_only, sender, subject, byline, email_text, story_t
         if attachment:
             email.add_attachment(attachment) 
         email.send()
-        time.sleep(5)
+        time.sleep(1)
